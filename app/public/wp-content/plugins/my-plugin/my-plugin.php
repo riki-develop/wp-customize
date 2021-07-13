c2307c8383116d20dfecdfb4cd2a3fd057841b59
riki karasawa <fabulous.riki@gmail.com>
2021-07-14 08:34:37 +0900

    自作プラグイン作成

---
+++ app/public/wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Plugin Name: マイプラグイン
+ * Version: 1.0
+ * Description: テストプラグイン
+ * Author: Karasawa
+*/
+
+// ショートコードを定義
+add_shortcode('date', function() {
+	return date('Y年 n月 j日 H:i:s');
+});
+
+// パラメータを渡すショートコード記述例
+add_shortcode('sum', function($atts) {
+	// パラメータが空の場合はデフォルト値を返す
+	$atts = shortcode_atts([
+		'x' => 0,
+		'y' => 0
+	], $atts, 'sum');
+
+	return $atts['x'] + $atts['y'];
+});
+
+// カスタム投稿追加の定義
+add_action('init', function() {
+	register_post_type('item', [
+		'label' => '商品',
+		'public' => true,
+		'menu_position' => 5,
+		'menu_icon' => 'dashicons-store',
+		'supports' => ['thumbnail', 'title', 'editor', 'custom-fields'],
+		'has_archive' => true,
+		'show_in_rest' => true,
+	]);
+	// カスタム分類定義
+	register_taxonomy('genre', 'item', [
+		'label' => '商品ジャンル',
+		'hierarchical' => true,
+		'show_in_rest' => true,
+	]);
+});
--- app/public/wp-content/themes/customtheme/functions.php
+++ app/public/wp-content/themes/customtheme/functions.php
@@ -7,40 +7,6 @@
  * @package customtheme
  */
 
-// ショートコードを定義
-add_shortcode('date', function() {
-	return date('Y年 n月 j日 H:i:s');
-});
-// パラメータを渡すショートコード記述例
-add_shortcode('sum', function($atts) {
-	// パラメータが空の場合はデフォルト値を返す
-	$atts = shortcode_atts([
-		'x' => 0,
-		'y' => 0
-	], $atts, 'sum');
-
-	return $atts['x'] + $atts['y'];
-});
-
-// カスタム投稿追加の定義
-add_action('init', function() {
-	register_post_type('item', [
-		'label' => '商品',
-		'public' => true,
-		'menu_position' => 5,
-		'menu_icon' => 'dashicons-store',
-		'supports' => ['thumbnail', 'title', 'editor', 'custom-fields'],
-		'has_archive' => true,
-		'show_in_rest' => true,
-	]);
-	// カスタム分類定義
-	register_taxonomy('genre', 'item', [
-		'label' => '商品ジャンル',
-		'hierarchical' => true,
-		'show_in_rest' => true,
-	]);
-});
-
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
